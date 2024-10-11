80e4b3dec9d7a4eaaa9a12420e1939f19d3e10d8
Dev-Kauan <kauanlima127@gmail.com>
2024-10-11 13:39:28 -0300

    feat: inicialização do index

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import NavbarHeader from "./components/NavBarHeader";
+
+export default function Page() {
+  return (
+    <NavbarHeader titulo="Inicial">
+      <h1>Página</h1>
+    </NavbarHeader>
+  );
+}
