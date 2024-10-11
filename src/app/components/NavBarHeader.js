ed8f36c62e78aba24250c76f25d0f84ffa0e39f9
Dev-Kauan <kauanlima127@gmail.com>
2024-10-11 13:39:13 -0300

    feat: Criação do component de navBarHeader

---
+++ src/app/components/NavBarHeader.js
@@ -0,0 +1,27 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+export default function NavBarHeader(props) {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" style={{ height: 100 }}>
+        <Container>
+          <Navbar.Brand href="/" className="me-auto">
+            <img src="" alt="" /> CineKauan
+          </Navbar.Brand>
+          <Nav className="mx-auto fs-5">
+            <Nav.Link href="/">Voos</Nav.Link>
+            <Nav.Link href="/aeroporto">Filmes</Nav.Link>
+            <Nav.Link href="/passagem">Ingressos</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto fs-5">
+            <Nav.Link href="/user/login">Login</Nav.Link>
+            <Nav.Link href="/user/form">Cadastre-se</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        {props.children}
+      </Container>
+    </>
+  );
+}
