1bc248fedf376548264468c672fe9cfb869fb25c
Dev-Kauan <kauanlima@gmail.com>
2024-10-12 19:33:00 -0300

    feat: Registro de usuário e autenticação

---
+++ src/app/users/login/page.js
@@ -0,0 +1,66 @@
+'use client'
+
+import { Formik } from "formik";
+import { useRouter } from "next/navigation";
+import { Button, Form } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
+
+export default function Page() {
+    const route = useRouter()
+
+    function autenticar(dados) {
+        if (dados.email === 'admin@gmail.com' && dados.senha === 'admin') {
+            alert('Acessou!');
+        }
+        // Buscar os usuários salvos no localStorage
+        const usuarios = JSON.parse(localStorage.getItem('users')) || [];
+
+        // Verificar se o usuário existe e se a senha está correta
+        const usuarioEncontrado = usuarios.find(usuario => usuario.email === dados.email);
+
+        if (usuarioEncontrado && usuarioEncontrado.senha === dados.senha) {
+            alert('Acesso permitido!');
+        } else {
+            alert('Email ou senha incorretos.');
+        }
+    }
+
+    return (
+        <Formik
+            initialValues={{ email: '', senha: '' }}
+            onSubmit={values => autenticar(values)}
+        >
+            {({
+                values,
+                handleChange,
+                handleSubmit,
+            }) => (
+                <Form className="mt-3">
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email"
+                            placeholder="ex: kauanCine@gmail.com"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange('email')}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="senha">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password"
+                            placeholder="Digite sua senha"
+                            name="senha"
+                            value={values.senha}
+                            onChange={handleChange('senha')}
+                        />
+                    </Form.Group>
+                    <div className="text-center">
+                        <Button variant="success" className="ms-1" onClick={handleSubmit}>
+                            <FaCheck />Login
+                        </Button>
+                    </div>
+                </Form>
+            )}
+        </Formik>
+    )
+}
---
+++ src/app/users/register/page.js
@@ -0,0 +1,88 @@
+'use client'
+
+import { Formik } from "formik";
+import { useRouter } from "next/navigation";
+import { Button, Form } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
+import { v4 } from "uuid";
+
+export default function Page() {
+    const route = useRouter()
+
+    const users = [];
+
+    function salvar(dados) {
+        dados.id = v4()
+        users.push(dados)
+        localStorage.setItem('users', JSON.stringify(users))
+        console.log(users);
+        return route.push('/users/login');
+    }
+
+    return (
+        <Formik
+            initialValues={{ nome: '', cpf: '', celular: '', email: '', senha: '' }}
+            onSubmit={values => salvar(values)}
+        >
+            {({
+                values,
+                handleChange,
+                handleSubmit,
+            }) => (
+                <Form className="mt-3">
+                    <Form.Group className="mb-3" controlId="nome">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Digite seu nome"
+                            name="nome"
+                            value={values.nome}
+                            onChange={handleChange('nome')}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="cpf">
+                        <Form.Label>CPF</Form.Label>
+                        <Form.Control type="text"
+                            placeholder="Digite o seu CPF"
+                            name="cpf"
+                            value={values.cpf}
+                            onChange={handleChange('cpf')}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="celular">
+                        <Form.Label>Celular</Form.Label>
+                        <Form.Control type="text"
+                            placeholder="Digite o seu número de telefone"
+                            name="celular"
+                            value={values.celular}
+                            onChange={handleChange('celular')}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email"
+                            placeholder="ex: kauanCine@gmail.com"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange('email')}
+                        />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="senha">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password"
+                            placeholder="Digite sua senha"
+                            name="senha"
+                            value={values.senha}
+                            onChange={handleChange('senha')}
+                        />
+                    </Form.Group>
+                    <div className="text-center">
+                        <Button variant="success" className="ms-1" onClick={handleSubmit}>
+                            <FaCheck />Salvar
+                        </Button>
+                    </div>
+                </Form>
+            )}
+        </Formik>
+    )
+}
