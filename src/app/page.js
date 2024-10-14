0ee596a50d1a21cb1c53cf51f960813ea491d28a
Dev-Kauan <kauanlima127@gmail.com>
2024-10-14 13:25:55 -0300

    Adicionando o cards de filmes no page inicial.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 'use client'
+import CardsFilmes from "./components/CardsFilmes";
 import NavbarHeader from "./components/NavBarHeader";
 import SwipperCarrossel from "./components/SwipperCarrossel";
 
 export default function Page() {
+
   return (
     <NavbarHeader titulo="Inicial">
       <SwipperCarrossel></SwipperCarrossel>
+      <CardsFilmes></CardsFilmes>
     </NavbarHeader>
   );
 }
