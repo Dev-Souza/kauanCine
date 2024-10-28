29eff357b1221a2f43640545bfe0f25b9ad15d73
Dev-Kauan <kauanlima@gmail.com>
2024-10-28 19:02:09 -0300

    feat: Máscaras para criação de user.

--- src/app/users/register/page.js
+++ src/app/users/register/page.js
@@ -4,6 +4,7 @@ import { Formik } from "formik";
 import { useRouter } from "next/navigation";
 import { Button, Form } from "react-bootstrap";
 import { FaCheck } from "react-icons/fa";
+import { mask } from "remask";
 import Swal from "sweetalert2";
 import { v4 } from "uuid";
 
@@ -33,71 +34,79 @@ export default function Page() {
                 values,
                 handleChange,
                 handleSubmit,
-            }) => (
-                <Form className="mt-3">
-                    <Form.Group className="mb-3" controlId="nome">
-                        <Form.Label>Nome</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Digite seu nome"
-                            name="nome"
-                            value={values.nome}
-                            onChange={handleChange('nome')}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="data_nascimento">
-                        <Form.Label>Data Nascimento:</Form.Label>
-                        <Form.Control
-                            type="date"
-                            placeholder="Digite seu data_nascimento"
-                            name="data_nascimento"
-                            value={values.data_nascimento}
-                            onChange={handleChange('nome')}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="cpf">
-                        <Form.Label>CPF</Form.Label>
-                        <Form.Control type="text"
-                            placeholder="Digite o seu CPF"
-                            name="cpf"
-                            value={values.cpf}
-                            onChange={handleChange('cpf')}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="celular">
-                        <Form.Label>Celular</Form.Label>
-                        <Form.Control type="tel"
-                            placeholder="Digite o seu número de telefone"
-                            name="celular"
-                            value={values.celular}
-                            onChange={handleChange('celular')}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email"
-                            placeholder="ex: kauanCine@gmail.com"
-                            name="email"
-                            value={values.email}
-                            onChange={handleChange('email')}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="senha">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password"
-                            placeholder="Digite sua senha"
-                            name="senha"
-                            value={values.senha}
-                            onChange={handleChange('senha')}
-                        />
-                    </Form.Group>
-                    <div className="text-center">
-                        <Button variant="success" className="ms-1" onClick={handleSubmit}>
-                            <FaCheck />Salvar
-                        </Button>
-                    </div>
-                </Form>
-            )}
+            }) => {
+
+                //Máscara do meu CPF
+                values.cpf = mask(values.cpf, "999.999.999-99");
+
+                //Máscara do meu celular
+                values.celular = mask(values.celular, "(99) 99999-9999")
+
+                return (
+                    <Form className="mt-3">
+                        <Form.Group className="mb-3" controlId="nome">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Digite seu nome"
+                                name="nome"
+                                value={values.nome}
+                                onChange={handleChange('nome')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="data_nascimento">
+                            <Form.Label>Data Nascimento:</Form.Label>
+                            <Form.Control
+                                type="date"
+                                name="data_nascimento"
+                                value={values.data_nascimento}
+                                onChange={handleChange('data_nascimento')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="cpf">
+                            <Form.Label>CPF</Form.Label>
+                            <Form.Control type="text"
+                                placeholder="Digite o seu CPF"
+                                name="cpf"
+                                value={values.cpf}
+                                onChange={handleChange('cpf')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="celular">
+                            <Form.Label>Celular</Form.Label>
+                            <Form.Control type="tel"
+                                placeholder="Digite o seu número de telefone"
+                                name="celular"
+                                value={values.celular}
+                                onChange={handleChange('celular')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email"
+                                placeholder="ex: kauanCine@gmail.com"
+                                name="email"
+                                value={values.email}
+                                onChange={handleChange('email')}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="senha">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password"
+                                placeholder="Digite sua senha"
+                                name="senha"
+                                value={values.senha}
+                                onChange={handleChange('senha')}
+                            />
+                        </Form.Group>
+                        <div className="text-center">
+                            <Button variant="success" className="ms-1" onClick={handleSubmit}>
+                                <FaCheck />Salvar
+                            </Button>
+                        </div>
+                    </Form>
+                )
+            }}
         </Formik>
     )
 }
