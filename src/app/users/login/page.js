2190eaa63f57a05361521cb96dd4a635b817a8fb
Dev-Kauan <kauanlima@gmail.com>
2024-10-28 19:04:59 -0300

    feat: Corrigindo o acesso de admin e adicionando o sweet alert.

--- src/app/users/login/page.js
+++ src/app/users/login/page.js
@@ -4,12 +4,13 @@ import { Formik } from "formik";
 import { useRouter } from "next/navigation";
 import { Button, Form } from "react-bootstrap";
 import { FaCheck } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 export default function Page() {
     const route = useRouter()
 
     function autenticar(dados) {
-        if (dados.email === 'admin@gmail.com' && dados.senha === 'admin') {
+        if (dados.email === 'admin' && dados.senha === 'admin') {
             route.push('/admin')
         }
         // Buscar os usuários salvos no localStorage
@@ -19,9 +20,13 @@ export default function Page() {
         const usuarioEncontrado = usuarios.find(usuario => usuario.email === dados.email);
 
         if (usuarioEncontrado && usuarioEncontrado.senha === dados.senha) {
-            alert('Acesso permitido!');
+            route.push('/')
         } else {
-            alert('Email ou senha incorretos.');
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Email ou senha incorretos",
+              });
         }
     }
 
